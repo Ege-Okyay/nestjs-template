3b84547190f3b17c3ab21f43cc328303b582804f
Ege-Okyay <egeokyay0@gmail.com>
2024-12-17 20:54:56 +0300

    deleted unnecessary code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
 import { LoggingMiddleware } from './middlewares/logging.middleware';
-import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 
 @Module({
